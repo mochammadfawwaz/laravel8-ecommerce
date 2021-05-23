9027cab8731338fbf59f5383e1dceddd877120c7
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-05-23 14:50:36 +0700

    14. admin Add new Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
@@ -49,6 +50,7 @@ Route::get('/search',SearchComponent::class) -> name('product.search');
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
 });
 
 // for user or customer
